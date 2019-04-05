78809997ba1607608f82b5fea31cbd68a3e75c25
Ewan Harris <ewanharris93@gmail.com>
2019-04-05 16:04:14 +0100

    test: fixup test syntax, reenable view autocomplete tests

--- spec/viewAutoCompleteProvider-spec.js
+++ spec/viewAutoCompleteProvider-spec.js
@@ -1,16 +1,15 @@
 'use babel';
 
-/* eslint-env mocha */
-/* global waitsForPromise */
-
 import viewAutoCompleteProvider from '../lib/providers/viewAutoCompleteProvider';
+import path from 'path';
+import Project from '../lib/project';
 
-let editor;
+let editor, atomEnvironment;
 
 // eslint-disable-next-line no-unused-vars
 function initTextEditor(text) {
-	editor = atom.workspace.buildTextEditor();
-	editor.setGrammar(atom.grammars.grammarForScopeName('text.alloyxml'));
+	editor = atomEnvironment.workspace.buildTextEditor();
+	editor.setGrammar(atomEnvironment.grammars.grammarForScopeName('text.alloyxml'));
 	editor.insertText(text);
 }
 
@@ -24,77 +23,82 @@ function getSuggestions(prefix) {
 	});
 }
 
-beforeEach(() => {
-	waitsForPromise(() =>
-		atom.packages.activatePackage('appcelerator-titanium')
-	);
-});
-
 describe('Tag suggestions', function () {
 
-	// it('should provide tag suggestions', function () {
-	// 	initTextEditor('<W');
-	// 	suggestions = getSuggestions('W');
+	beforeEach(async () => {
+		atomEnvironment = global.buildAtomEnvironment();
+		await atomEnvironment.packages.activatePackage(path.join(__dirname, '..'));
+	});
 
-	// 	expect(suggestions.length).toBe(4);
+	it('should provide tag suggestions', function () {
+		Project.isTitaniumApp = true;
 
-	// 	expect(suggestions[0].type).toBe('tag');
-	// 	expect(suggestions[0].displayText).toBe('WebView');
-	// 	expect(suggestions[0].snippet).toBe('WebView$1>$2</WebView>');
-	// 	expect(suggestions[0].rightLabel).toBe('Ti.UI.WebView');
-	// 	expect(suggestions[0].description).toBe('Ti.UI.WebView: The web view allows you to open an HTML5 based view which can load either local or remote content. ');
-	// 	expect(suggestions[0].descriptionMoreURL).toBe('http://docs.appcelerator.com/platform/latest/#!/api/Titanium.UI.WebView');
+		initTextEditor('<W');
+		const suggestions = getSuggestions('W');
 
-	// 	expect(suggestions[1].type).toBe('tag');
-	// 	expect(suggestions[1].displayText).toBe('Widget');
-	// 	expect(suggestions[1].snippet).toBe('Widget$1>$2</Widget>');
-	// 	expect(suggestions[1].rightLabel).toBe('Alloy.Widget');
-	// 	expect(suggestions[1].description).toBe('Alloy.Widget: Widgets are self-contained components that can be easily dropped into an Alloy project.');
-	// 	expect(suggestions[1].descriptionMoreURL).toBe('http://docs.appcelerator.com/platform/latest/#!/api/Alloy.Widget');
+		expect(suggestions.length).to.equal(4);
 
-	// 	expect(suggestions[2].type).toBe('tag');
-	// 	expect(suggestions[2].displayText).toBe('Window');
-	// 	expect(suggestions[2].snippet).toBe('Window$1>$2</Window>');
-	// 	expect(suggestions[2].rightLabel).toBe('Ti.UI.Window');
-	// 	expect(suggestions[2].description).toBe('Ti.UI.Window: The Window is an empty drawing surface or container.');
-	// 	expect(suggestions[2].descriptionMoreURL).toBe('http://docs.appcelerator.com/platform/latest/#!/api/Titanium.UI.Window');
+		expect(suggestions[0].type).to.equal('tag');
+		expect(suggestions[0].displayText).to.equal('WebView');
+		expect(suggestions[0].snippet).to.equal('WebView$1>$2</WebView>');
+		expect(suggestions[0].rightLabel).to.equal('Ti.UI.WebView');
+		expect(suggestions[0].description).to.equal('Ti.UI.WebView: The web view allows you to open an HTML5 based view which can load either local or remote content.');
+		expect(suggestions[0].descriptionMoreURL).to.equal('http://docs.appcelerator.com/platform/latest/#!/api/Titanium.UI.WebView');
 
-	// 	expect(suggestions[3].type).toBe('tag');
-	// 	expect(suggestions[3].displayText).toBe('WindowToolbar');
-	// 	expect(suggestions[3].snippet).toBe('WindowToolbar$1>$2</WindowToolbar>');
-	// 	expect(suggestions[3].rightLabel).toBe('Ti.UI.Window.WindowToolbar');
-	// 	expect(suggestions[3].description).toBe('Ti.UI.Window.WindowToolbar');
-	// 	expect(suggestions[3].descriptionMoreURL).toBe('http://docs.appcelerator.com/platform/latest/#!/api/Titanium.UI.Window.WindowToolbar');
-	// });
+		expect(suggestions[1].type).to.equal('tag');
+		expect(suggestions[1].displayText).to.equal('Widget');
+		expect(suggestions[1].snippet).to.equal('Widget$1>$2</Widget>');
+		expect(suggestions[1].rightLabel).to.equal('Alloy.Widget');
+		expect(suggestions[1].description).to.equal('Alloy.Widget: Widgets are self-contained components that can be easily dropped into an Alloy project.');
+		expect(suggestions[1].descriptionMoreURL).to.equal('http://docs.appcelerator.com/platform/latest/#!/api/Alloy.Widget');
+
+		expect(suggestions[2].type).to.equal('tag');
+		expect(suggestions[2].displayText).to.equal('Window');
+		expect(suggestions[2].snippet).to.equal('Window$1>$2</Window>');
+		expect(suggestions[2].rightLabel).to.equal('Ti.UI.Window');
+		expect(suggestions[2].description).to.equal('Ti.UI.Window: The Window is an empty drawing surface or container.');
+		expect(suggestions[2].descriptionMoreURL).to.equal('http://docs.appcelerator.com/platform/latest/#!/api/Titanium.UI.Window');
+
+		expect(suggestions[3].type).to.equal('tag');
+		expect(suggestions[3].displayText).to.equal('WindowToolbar');
+		expect(suggestions[3].snippet).to.equal('WindowToolbar$1>$2</WindowToolbar>');
+		expect(suggestions[3].rightLabel).to.equal('Ti.UI.Window.WindowToolbar');
+		expect(suggestions[3].description).to.equal('Ti.UI.Window.WindowToolbar');
+		expect(suggestions[3].descriptionMoreURL).to.equal('http://docs.appcelerator.com/platform/latest/#!/api/Titanium.UI.Window.WindowToolbar');
+	});
 });
 
 describe('Attribute suggestions', function () {
 
-	// it('should provide property suggestions', function () {
-	// 	initTextEditor('<Window s');
-	// 	suggestions = getSuggestions('status');
+	it('should provide property suggestions', function () {
+		Project.isTitaniumApp = true;
 
-	// 	expect(suggestions.length).toBe(7);
+		initTextEditor('<Window s');
+		const suggestions = getSuggestions('status');
 
-	// 	expect(suggestions[0].type).toBe('property');
-	// 	expect(suggestions[0].displayText).toBe('scaleX');
-	// 	expect(suggestions[0].snippet).toBe('scaleX="$1"$0');
-	// 	expect(suggestions[0].rightLabel).toBe('Window');
-	// 	expect(suggestions[0].description).toBe('Ti.UI.Window: Scaling of the view in x-axis in pixels.');
-	// 	expect(suggestions[0].descriptionMoreURL).toBe('http://docs.appcelerator.com/platform/latest/#!/api/Titanium.UI.Window-property-scaleX');
-	// });
+		expect(suggestions.length).to.equal(8);
 
-	// it('should provide event suggestions for', function () {
-	// 	initTextEditor('<Window on');
-	// 	suggestions = getSuggestions('on');
+		expect(suggestions[0].type).to.equal('property');
+		expect(suggestions[0].displayText).to.equal('scaleX');
+		expect(suggestions[0].snippet).to.equal('scaleX="$1"$0');
+		expect(suggestions[0].rightLabel).to.equal('Window');
+		expect(suggestions[0].description).to.equal('Ti.UI.Window: Scaling of the view in x-axis in pixels.');
+		expect(suggestions[0].descriptionMoreURL).to.equal('http://docs.appcelerator.com/platform/latest/#!/api/Titanium.UI.Window-property-scaleX');
+	});
 
-	// 	expect(suggestions.length).toBe(35);
+	it('should provide event suggestions for', function () {
+		Project.isTitaniumApp = true;
 
-	// 	expect(suggestions[0].type).toBe('function');
-	// 	expect(suggestions[0].displayText).toBe('onAndroidback');
-	// 	expect(suggestions[0].snippet).toBe('onAndroidback="$1"$0');
-	// 	expect(suggestions[0].rightLabel).toBe('Window');
-	// 	expect(suggestions[0].description).toBe('Ti.UI.Window: androidback event');
-	// 	expect(suggestions[0].descriptionMoreURL).toBe('http://docs.appcelerator.com/platform/latest/#!/api/Titanium.UI.Window-event-androidback');
-	// });
+		initTextEditor('<Window on');
+		const suggestions = getSuggestions('on');
+
+		expect(suggestions.length).to.equal(35);
+
+		expect(suggestions[0].type).to.equal('function');
+		expect(suggestions[0].displayText).to.equal('onAndroidback');
+		expect(suggestions[0].snippet).to.equal('onAndroidback="$1"$0');
+		expect(suggestions[0].rightLabel).to.equal('Window');
+		expect(suggestions[0].description).to.equal('Ti.UI.Window: androidback event');
+		expect(suggestions[0].descriptionMoreURL).to.equal('http://docs.appcelerator.com/platform/latest/#!/api/Titanium.UI.Window-event-androidback');
+	});
 });
