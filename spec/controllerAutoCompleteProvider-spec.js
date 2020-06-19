943590b7bbe403ae33b94e7216ab9f20893f672c
Ewan Harris <ewanharris93@gmail.com>
2020-06-19 09:35:31 +0100

    test: fix tests for newer atom versions

--- spec/controllerAutoCompleteProvider-spec.js
+++ spec/controllerAutoCompleteProvider-spec.js
@@ -6,6 +6,7 @@ import Project from '../lib/project';
 import * as sinon from 'sinon';
 import * as tce from 'titanium-editor-commons';
 import * as fs from 'fs';
+import * as semver from 'semver';
 
 let editor, atomEnvironment, sandbox;
 
@@ -58,16 +59,29 @@ describe('Ti namespace suggestions', function () {
 		expect(suggestions[0].api).to.equal('Ti');
 		expect(suggestions[0].replacementPrefix).to.equal('Ti.');
 
-		expect(suggestions[1].type).to.equal('method');
-		expect(suggestions[1].text).to.equal('Ti.XML');
-		expect(suggestions[1].api).to.equal('Ti');
-		expect(suggestions[1].replacementPrefix).to.equal('Ti.');
+		// TODO: Remove this check when support for Atom 1.46 and lower is dropped
+		if (semver.gte(process.version, '12.0.0')) {
+			expect(suggestions[1].type).to.equal('method');
+			expect(suggestions[1].text).to.equal('Ti.API');
+			expect(suggestions[1].api).to.equal('Ti');
+			expect(suggestions[1].replacementPrefix).to.equal('Ti.');
 
-		expect(suggestions[2].type).to.equal('method');
-		expect(suggestions[2].text).to.equal('Ti.API');
-		expect(suggestions[2].api).to.equal('Ti');
-		expect(suggestions[2].replacementPrefix).to.equal('Ti.');
+			expect(suggestions[2].type).to.equal('method');
+			expect(suggestions[2].text).to.equal('Ti.App');
+			expect(suggestions[2].api).to.equal('Ti');
+			expect(suggestions[2].replacementPrefix).to.equal('Ti.');
+		} else {
 
+			expect(suggestions[1].type).to.equal('method');
+			expect(suggestions[1].text).to.equal('Ti.XML');
+			expect(suggestions[1].api).to.equal('Ti');
+			expect(suggestions[1].replacementPrefix).to.equal('Ti.');
+
+			expect(suggestions[2].type).to.equal('method');
+			expect(suggestions[2].text).to.equal('Ti.API');
+			expect(suggestions[2].api).to.equal('Ti');
+			expect(suggestions[2].replacementPrefix).to.equal('Ti.');
+		}
 	});
 });
 
@@ -97,13 +111,24 @@ describe('Extended Ti suggestions', function () {
 
 		expect(suggestions.length).to.equal(73);
 
-		expect(suggestions[0].type).to.equal('properties');
-		expect(suggestions[0].displayText).to.equal('Ti.UI.tintColor');
-		expect(suggestions[0].descriptionMoreURL).to.equal('http://docs.appcelerator.com/platform/latest/#!/api/Titanium');
+		// TODO: Remove this check when support for Atom 1.46 and lower is dropped
+		if (semver.gte(process.version, '12.0.0')) {
+			expect(suggestions[0].type).to.equal('function');
+			expect(suggestions[0].displayText).to.equal('Ti.UI.fireEvent');
+			expect(suggestions[0].descriptionMoreURL).to.equal('http://docs.appcelerator.com/platform/latest/#!/api/Titanium');
 
-		expect(suggestions[1].type).to.equal('function');
-		expect(suggestions[1].displayText).to.equal('Ti.UI.createTab');
-		expect(suggestions[1].descriptionMoreURL).to.equal('http://docs.appcelerator.com/platform/latest/#!/api/Titanium');
+			expect(suggestions[1].type).to.equal('function');
+			expect(suggestions[1].displayText).to.equal('Ti.UI.createTab');
+			expect(suggestions[1].descriptionMoreURL).to.equal('http://docs.appcelerator.com/platform/latest/#!/api/Titanium');
+		} else {
+			expect(suggestions[0].type).to.equal('properties');
+			expect(suggestions[0].displayText).to.equal('Ti.UI.tintColor');
+			expect(suggestions[0].descriptionMoreURL).to.equal('http://docs.appcelerator.com/platform/latest/#!/api/Titanium');
+
+			expect(suggestions[1].type).to.equal('function');
+			expect(suggestions[1].displayText).to.equal('Ti.UI.createTab');
+			expect(suggestions[1].descriptionMoreURL).to.equal('http://docs.appcelerator.com/platform/latest/#!/api/Titanium');
+		}
 
 	});
 });
@@ -140,18 +165,29 @@ describe('Alloy namespace suggestions', function () {
 		expect(suggestions[0].api).to.equal('Alloy');
 		expect(suggestions[0].replacementPrefix).to.equal('Alloy.');
 
-		expect(suggestions[1].type).to.equal('properties');
-		expect(suggestions[1].displayText).to.equal('Alloy.CFG');
-		expect(suggestions[1].rightLabel).to.equal('Alloy');
-		expect(suggestions[1].snippet).to.equal('CFG');
-		expect(suggestions[1].priority).to.equal(2);
+		// TODO: Remove this check when support for Atom 1.46 and lower is dropped
+		if (semver.gte(process.version, '12.0.0')) {
+			expect(suggestions[1].type).to.equal('properties');
+			expect(suggestions[1].displayText).to.equal('Alloy.CFG');
+			expect(suggestions[1].rightLabel).to.equal('Alloy');
+			expect(suggestions[1].snippet).to.equal('CFG');
+			expect(suggestions[1].priority).to.equal(2);
+			console.log(suggestions[2]);
+			expect(suggestions[2].type).to.equal('method');
+			expect(suggestions[2].displayText).to.equal('Alloy.Models');
+		} else {
+			expect(suggestions[1].type).to.equal('properties');
+			expect(suggestions[1].displayText).to.equal('Alloy.CFG');
+			expect(suggestions[1].rightLabel).to.equal('Alloy');
+			expect(suggestions[1].snippet).to.equal('CFG');
+			expect(suggestions[1].priority).to.equal(2);
 
-		expect(suggestions[2].type).to.equal('properties');
-		expect(suggestions[2].displayText).to.equal('Alloy.Models');
-		expect(suggestions[2].rightLabel).to.equal('Alloy');
-		expect(suggestions[2].snippet).to.equal('Models');
-		expect(suggestions[2].priority).to.equal(2);
-
+			expect(suggestions[2].type).to.equal('properties');
+			expect(suggestions[2].displayText).to.equal('Alloy.Models');
+			expect(suggestions[2].rightLabel).to.equal('Alloy');
+			expect(suggestions[2].snippet).to.equal('Models');
+			expect(suggestions[2].priority).to.equal(2);
+		}
 	});
 });
 
@@ -199,17 +235,31 @@ describe('Extended Alloy suggestions', function () {
 
 		expect(suggestions.length).to.equal(12);
 
-		expect(suggestions[0].type).to.equal('function');
-		expect(suggestions[0].displayText).to.equal('Alloy.Controller.getView');
-		// eslint-disable-next-line no-template-curly-in-string
-		expect(suggestions[0].snippet).to.equal('getView(${1})${0}');
-		expect(suggestions[0].rightLabel).to.equal('Controller');
+		// TODO: Remove this check when support for Atom 1.46 and lower is dropped
+		if (semver.gte(process.version, '12.0.0')) {
+			expect(suggestions[0].type).to.equal('function');
+			expect(suggestions[0].displayText).to.equal('Alloy.Controller.destroy');
+			// eslint-disable-next-line no-template-curly-in-string
+			expect(suggestions[0].snippet).to.equal('destroy(${1})${0}');
+			expect(suggestions[0].rightLabel).to.equal('Controller');
 
-		expect(suggestions[2].type).to.equal('function');
-		expect(suggestions[2].displayText).to.equal('Alloy.Controller.getViews');
-		// eslint-disable-next-line no-template-curly-in-string
-		expect(suggestions[2].snippet).to.equal('getViews(${1})${0}');
-		expect(suggestions[2].rightLabel).to.equal('Controller');
+			expect(suggestions[2].type).to.equal('function');
+			expect(suggestions[2].displayText).to.equal('Alloy.Controller.addClass');
+			// eslint-disable-next-line no-template-curly-in-string
+			expect(suggestions[2].snippet).to.equal('addClass(${1})${0}');
+			expect(suggestions[2].rightLabel).to.equal('Controller');
+		} else {
+			expect(suggestions[0].type).to.equal('function');
+			expect(suggestions[0].displayText).to.equal('Alloy.Controller.getView');
+			// eslint-disable-next-line no-template-curly-in-string
+			expect(suggestions[0].snippet).to.equal('getView(${1})${0}');
+			expect(suggestions[0].rightLabel).to.equal('Controller');
 
+			expect(suggestions[2].type).to.equal('function');
+			expect(suggestions[2].displayText).to.equal('Alloy.Controller.getViews');
+			// eslint-disable-next-line no-template-curly-in-string
+			expect(suggestions[2].snippet).to.equal('getViews(${1})${0}');
+			expect(suggestions[2].rightLabel).to.equal('Controller');
+		}
 	});
 });
