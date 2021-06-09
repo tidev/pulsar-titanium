4ccc3d02ffe40226d4bfb7d7aefecddf32204e2f
Ewan Harris <ewanharris93@gmail.com>
2021-06-09 17:04:48 +0100

    feat(toolbar): update liveview icon to better distinguish states
    As the octicons bundled in atom is very outdated, this adds a new octicon class that pulls in the
    latest octicons version and allows us to use newer icons
    
    Closes #306

---
+++ lib/ui/octicon.jsx
@@ -0,0 +1,76 @@
+/** @babel */
+/** @jsx etch.dom */
+
+import etch from 'etch';
+import octicons from '@primer/octicons';
+
+export class Octicon {
+	/**
+	 *
+	 * @param {Object} opts - Options
+	 * @param {String} opts.name - The octicons icon name
+	 * @param {String[]} opts.classNames - Extra classnames to include
+	 * @param {Boolean} opts.spin - Whether the icon should spin
+	 * @param {Boolean} opts.disabled - Whether the icon should be in disabled state, also stops also handlers
+	 * @param {String} opts.title - Title for the tooltip
+	 */
+	constructor(opts) {
+		if (!opts || !opts.name) {
+			throw new Error('A name is required');
+		}
+
+		const isValid = octicons[opts.name];
+		if (!isValid) {
+			throw new Error(`${opts.name} is not a valid octicon`);
+		}
+
+		if (opts.spin) {
+			opts.spin = true;
+		}
+
+		this.opts = opts;
+
+		etch.initialize(this);
+		Octicon.setScheduler(atom.views);
+	}
+
+	static getScheduler () {
+		return etch.getScheduler();
+	}
+
+	static setScheduler (scheduler) {
+		etch.setScheduler(scheduler);
+	}
+
+	update (opts) {
+		const oldOpts = this.opts;
+		this.opts = Object.assign({}, oldOpts, opts);
+		return etch.update(this);
+	}
+
+	render () {
+		const { className, disabled, name, spin, title } = this.opts;
+		const classNames = [ 'etch-octicon', `etch-octicon-${name}` ];
+
+		if (spin) {
+			classNames.push('spin-etch-octicon');
+		}
+
+		if (className) {
+			classNames.push(className);
+		}
+
+		if (disabled) {
+			classNames.push('disabled');
+		}
+
+		const fill = disabled ? '#505868' : '#9da5b4';
+		const octicon = octicons[name].toSVG({ fill });
+
+		const handlers = disabled ? {} : { click: this.opts.click };
+
+		return (
+			<span innerHTML={octicon} className={classNames.join(' ')} on={handlers} style={{ display: 'flex', alignItems: 'center' }} title={title} />
+		);
+	}
+}
