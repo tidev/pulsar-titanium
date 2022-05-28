2ff97db46ad00e4e775a4a82d244ecd724318e8d
m1ga <migamiga@web.de>
2022-05-28 12:03:09 +0200

    keep console closed at startup

--- lib/ui/console.jsx
+++ lib/ui/console.jsx
@@ -119,7 +119,7 @@ export default class Console {
 		this.subscriptions.add(
 			atom.workspace.onDidOpen(e => {
 				if (e.item === this) {
-					this.state.isHidden = false;
+					this.state.isHidden = true;
 				}
 			})
 		);
